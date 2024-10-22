379fcbb43ad5583b0517443091280d6c97677fd5
ltfranks <ltfranks@calpoly.edu>
2024-10-22 10:21:50 -0700

    working on shadowdom

---
+++ packages/proto/public/scripts/grocery-link.js
@@ -0,0 +1,12 @@
+
+// want to add the cal poly import
+import {html} from "@calpoly/mustang";
+
+export class GroceryLink extends HTMLElement {
+    static template = html'<template>' +
+
+    constructor() {
+        super();
+
+    }
+}
